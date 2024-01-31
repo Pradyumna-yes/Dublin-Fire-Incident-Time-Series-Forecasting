5a0ba872d756e77807090b4584f3223ace0b629c
Pradyumna S <96283346+Pradyumna-yes@users.noreply.github.com>
2024-01-31 18:39:45 +0000

    Delete monthly_incident_count.sql

--- monthly_incident_count.sql
+++
@@ -1,6 +0,0 @@
-SELECT 
-    FORMAT(CONVERT(datetime, Date, 103), 'yyyy-MM') as Month, 
-    COUNT(*) as IncidentCount 
-FROM Incidents 
-GROUP BY FORMAT(CONVERT(datetime, Date, 103), 'yyyy-MM') 
-ORDER BY Month;
